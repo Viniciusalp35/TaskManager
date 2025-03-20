1f45a1c6468d252875e95c9bae8414fbdbc6d13f
Vinícius Lôbo <vinicius.alp5626@gmail.com>
2025-03-20 14:36:08 -0300

    Initial Commit

---
+++ TaskManager/TaskManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskManagerApp.swift
+//  TaskManager
+//
+//  Created by user on 20/03/25.
+//
+
+import SwiftUI
+
+@main
+struct TaskManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
